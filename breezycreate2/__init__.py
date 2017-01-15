3f4da7a7c699d4fbfe3c0b30e2ea60bf82fb4553
Mohamed Abdellatif <tee@Mohameds-MacBook-Pro.local>
2017-01-15 10:30:11 +0300

    Debugging...

--- breezycreate2/__init__.py
+++ breezycreate2/__init__.py
@@ -33,8 +33,8 @@ import math
 
 class Robot(object):
 
-    #def __init__(self, port='/dev/ttyUSB0', baud=115200):
-    def __init__(self, port='/dev/cu.usbserial-DA017X6T', baud=115200):
+    def __init__(self, port='/dev/ttyUSB0', baud=115200):
+    #def __init__(self, port='/dev/cu.usbserial-DA017X6T', baud=115200):
         '''
         Connects to the Create2 on the specified port at the specified baud rate.
         '''
