0d573929be022a7387c3672af5ec0f69b48e6e21
Mohamed Abdellatif <tee@Mohameds-MacBook-Pro.local>
2017-01-17 17:28:38 +0300

    Degbugging..

--- stage3.py
+++ stage3.py
@@ -30,7 +30,7 @@ def solve2():
         robot.updateBumpers()
         if robot.leftBump and robot.rightBump:
             robot.moveDistance(-0.05)    
-            if robot.bumpedRecently(3):
+            if robot.bumpedFrontRecently(3):
                 robot.rotate(180)
             else:
                 robot.rotate(-90)
